3661456db3f7f8dcbf3ccb3fe189aaf402adc677
YAN7 <904394373@qq.com>
2022-07-08 22:40:34 +0800

    add 7

---
+++ easy/7.readonly.ts
@@ -0,0 +1,32 @@
+/* _____________ Your Code Here _____________ */
+
+type MyReadonly<T> = {
+	readonly [key in keyof T]: T[key]
+}
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/7/answer
+  > View solutions: https://tsch.js.org/7/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
