3086f7a3cc8ec3a0f42667a3decc47623045de79
YAN7 <904394373@qq.com>
2022-07-14 16:37:30 +0800

    add 10

---
+++ medium/10.tuple-to-union.ts
@@ -0,0 +1,20 @@
+type TupleToUnion<T extends any[]> = T[number]
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/10/answer
+  > View solutions: https://tsch.js.org/10/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
