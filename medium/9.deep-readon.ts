fc4c8e1ebccc3fb2ce9df60736ec7ae1243ce018
YAN7 <904394373@qq.com>
2022-07-14 16:35:27 +0800

    add 9

---
+++ medium/9.deep-readon.ts
@@ -0,0 +1,70 @@
+/**
+ * * 关键在于用keyof T[key] extends never判断value是否是一个对象
+ */
+
+type DeepReadonly<T> = {
+	readonly [key in keyof T]: keyof T[key] extends never ? T[key] : DeepReadonly<T[key]>
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<DeepReadonly<X>, Expected>>,
+]
+
+type X = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type Expected = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/9/answer
+  > View solutions: https://tsch.js.org/9/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
